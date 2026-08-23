86a0048514d8dd10f0e3f800c34b1c66f517ff25
cbandit-0001 <charliebriley42@gmail.com>
2026-08-22 20:27:41 -0400

    unit testing - [8/22/26] part 1

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import pytest
+
+from src.db_builder import GetConnection
+
+# Paths
+TEST_DIR = Path(__file__).parent
+FIXTURES_DIR = TEST_DIR / "fixtures"
+TEST_YAML = FIXTURES_DIR / "test_query_templates.yaml"
+TEST_YAML_FAIL = FIXTURES_DIR / "test_query_templates_fail.yaml"
+
+
+@pytest.fixture(scope="session")
+def in_memory_db_init():
+    connector_cls = GetConnection.create_connection(connection_type='duckdb')
+    conn = connector_cls({'memory': True})
+    yield conn
---
+++ tests/unit/test_config.py
@@ -0,0 +1,20 @@
+import pytest
+
+from src.config.config import UPDATE_ROW, CREATE_TABLE, _load_queries, _QUERY_TEMPLATES_PATH
+from tests.conftest import TEST_YAML, TEST_YAML_FAIL
+
+
+def test_load_queries_pass():
+    _query: dict = _load_queries(TEST_YAML)
+    assert isinstance(_query, dict)
+    assert list(_query.keys()) == ['create_table']
+    assert len(_query.get('create_table')) == 344
+
+def test_load_queries_exception():
+    with pytest.raises(KeyError):
+        _query: dict = _load_queries(TEST_YAML_FAIL)
+
+def test_variables():
+    assert len(UPDATE_ROW) == 311
+    assert len(CREATE_TABLE) == 344
+    assert _QUERY_TEMPLATES_PATH.name == 'query_templates.yaml'
